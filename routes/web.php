d2d613741070cac11494b1adbda271bde735ffa5
Full Stack Developer(Web && Hybrid App) <denysmartin4952@gmail.com>
2023-11-27 01:14:40 -0800

    add/auth route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::get('{any}', function () {
     return view('app');
