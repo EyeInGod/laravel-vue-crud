5d1c078313df67f8cbc7d8180b04b2d2783abf08
Full Stack Developer(Web && Hybrid App) <denysmartin4952@gmail.com>
2023-11-27 01:14:27 -0800

    add/auth route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('{any}', function () {
     return view('app');
 })->where('any', '.*');
